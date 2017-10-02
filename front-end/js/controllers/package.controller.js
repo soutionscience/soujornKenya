6b78a65a2ef767d4cc6d2bcb68a678d50f0f4e5a
Rubin Njagi <rubinnjagi@yahoo.com>
2017-10-02 21:25:43 +0300

    added book now button

--- front-end/js/controllers/package.controller.js
+++ front-end/js/controllers/package.controller.js
@@ -12,8 +12,19 @@
 		$scope.dataset = [];
 		$scope.dataset = item;
 
-		console.log($scope.msg)
-		console.log(item)
+		// console.log($scope.msg)
+		console.log("what is in: " + $scope.dataset.Day1.Package)
+		console.log("what is in: " + $scope.dataset.Day1.image)
+
+		var isThereImage = function(index){
+			if($scope.dataset.index.image == null){
+				console.log("are you here: " +$scope.dataset.index.image)
+				return false;
+			}
+            console.log("are you here: " +$scope.dataset.index.image)
+           return true;
+			
+		}
 
 	};
 })();
